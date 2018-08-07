357d773464e42165c49b3a808432992287d2df23
DK <dwkim16@gmail.com>
2018-08-07 13:58:01 +0900

    - Ch.13 Finished

---
+++ ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Dowon Kim on 8/7/18.
+//  Copyright © 2018 Do Won Kim. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+}
